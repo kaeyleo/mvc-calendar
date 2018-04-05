9a5f7b6395d61b6dc6159cc7672e915042a97e9e
liaokeyu <934460541@qq.com>
2018-04-05 23:38:24 +0800

    feat: add view

---
+++ src/view.js
@@ -0,0 +1,8 @@
+export default class View {
+  constructor () {
+    this.$monthLabel = document.querySelector('.month-label')
+    this.$prevBtn = document.querySelector('.prev-btn')
+    this.$nextBtn = document.querySelector('.next-btn')
+    console.log('this is View')
+  }
+}
