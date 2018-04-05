7fb09bd646d769a9db6804fee7d9753568def51d
liaokeyu <934460541@qq.com>
2018-04-05 21:36:12 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  watch: true,
+  devServer: {
+    contentBase: './'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      }
+    ]
+  }
+}
